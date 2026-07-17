9f57a89538e16e35579e990e4630497eabcdf662
rupesh-hub <mahanta.rupesh704@gmail.com>
2026-07-17 19:31:27 +0530

    feat: added logger functionality

--- gateway/cmd/server/main.go
+++ gateway/cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/RupeshMahanta1994/flightops/shared/config"
+	"github.com/RupeshMahanta1994/flightops/shared/logger"
 )
 
 func main() {
@@ -20,4 +21,9 @@ func main() {
 	fmt.Println("DB Name        :", cfg.Database.Name)
 
 	fmt.Println("RabbitMQ URL   :", cfg.RabbitMQ.URL)
+
+	log := logger.New("gateway")
+	log.Info("Application Server started")
+	log.Warn("This is a warning")
+	log.Error("Unable to connect to database")
 }
--- shared/logger/logger.go
+++ shared/logger/logger.go
@@ -1,6 +1,20 @@
 package logger
 
+import (
+	"log/slog"
+	"os"
+)
+
 // New()
+func New(service string) *slog.Logger {
+	opts := &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}
+	handler := slog.NewJSONHandler(os.Stdout, opts)
+	return slog.New(handler).With(
+		slog.String("service", service),
+	)
+}
 
 // Get()
 
