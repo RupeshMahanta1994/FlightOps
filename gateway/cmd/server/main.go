baa234a31c4148c5c2beab4b980460885c5ad40d
rupesh-hub <mahanta.rupesh704@gmail.com>
2026-07-18 09:24:02 +0530

    fix: improve database connection error handling and logging

--- gateway/cmd/server/main.go
+++ gateway/cmd/server/main.go
@@ -30,12 +30,14 @@ func main() {
 	log := logger.New("gateway")
 	log.Info("Application Server started")
 	db, err := database.Init(cfg)
-	fmt.Println("Databse connection successful", db.Stats().OpenConnections, err)
+	if err != nil {
+		log.Error("Error connecting to DB", err)
+		db.Close()
+	}
+	log.Info("Databse connection successful", db.Stats().OpenConnections)
+	defer db.Close()
 
 	http.HandleFunc("/success", successHandler(log))
-	// http.HandleFunc("/bad-request", badRequestHandler(log))
-	// http.HandleFunc("/not-found", notFoundHandler(log))
-	// http.HandleFunc("/internal-error", internalErrorHandler(log))
 
 	log.Info("Gateway started", slog.String("port", cfg.App.Port))
 
@@ -51,40 +53,3 @@ func successHandler(log *slog.Logger) http.HandlerFunc {
 		log.Info("Successfully sent the response")
 	}
 }
-
-// func badRequestHandler(log *slog.Logger) http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		if err := response.BadRequest(
-// 			w,
-// 			"204",
-// 			"Passenger ID is required",
-// 		); err != nil {
-// 			log.Error("failed to write response", "error", err)
-// 		}
-// 	}
-// }
-
-// func notFoundHandler(log *slog.Logger) http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		if err := response.NotFound(
-// 			w,
-// 			"BOOKING_NOT_FOUND",
-// 			"Booking does not exist",
-// 		); err != nil {
-// 			log.Error("failed to write response", "error", err)
-// 		}
-// 	}
-// }
-
-// func internalErrorHandler(log *slog.Logger) http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		if err := response.InternalServerError(
-// 			w,
-// 			"DATABASE_ERROR",
-// 			"Unable to connect to database",
-// 		); err != nil {
-// 			log.Error("failed to write response", "error", err)
-// 		}
-// 	}
-
-// }
